a97b0cd2d1803808080a6074844efb50b38972e7
Nitin Tambe <tambenitin007@gmail.com>
2024-12-04 11:29:31 +0530

    Initial commit with existing programs

---
+++ APPLE  TAILWIND PROJECT/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
+}
+module.exports = {
+
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ]
+
+}
